f609df2a4ba3627059126d175a404fc433af6dfd
Usuario <Usuario@CPD02-M03>
2023-05-30 15:50:21 -0300

    adasda

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -10,10 +10,29 @@ namespace App1
 {
     public partial class MainPage : ContentPage
     {
+        private string conteudoArrastado;
         public MainPage()
         {
             InitializeComponent();
             juan.Source = ImageSource.FromResource("App1.cavalo.juan.jpg");
         }
-    }
+
+        private async void selecao_Drop(object sender, DropEventArgs e)
+        {
+
+            conteudoArrastado = juan.Source.ToString();
+
+            if (e.Data != null)
+            {
+                await DisplayAlert("Caballo", "sim funfa", "cavalo");
+            }
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+     
+    } 
 }
